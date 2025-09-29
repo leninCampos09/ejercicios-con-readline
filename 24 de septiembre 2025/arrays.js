59563f4337d088c2f163da3b7fe3736adfaacd4e
leninCampos09 <127224517+leninCampos09@users.noreply.github.com>
2025-09-29 08:46:33 -0600

    arrays

---
+++ 24 de septiembre 2025/arrays.js
@@ -0,0 +1,41 @@
+const numeros=[10,20,30,40,50];
+
+const meses = new Array('Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio');
+
+const deTodo= ['Hola', 10, true, 'si', null, {nombre: 'juan', trabajo: 'programador'}, [1,2,3]];
+// console.table(numeros);
+// console.table(meses);
+// console.table(deTodo);
+
+// //imprimir el true
+// console.log(deTodo[2]);
+
+// //imprimir el nombre de juan
+// console.log(deTodo[5].nombre);
+
+// //imprimir el 2
+// console.log(deTodo[6][1]);
+
+// //modificar el numero 20 por 80
+// numeros[1]=80;
+// console.log(numeros);
+
+// meses.push('Julio'); //agrega al final
+// console.log(meses);
+
+// meses.pop();
+// console.log(meses);
+
+// meses.shift();
+// console.log(meses);
+
+// meses.unshift('Enero');
+// console.log(meses);
+
+//imprimir enero, febrero y marzo en el mismo console.log
+for(let i=0;i<meses.length;i++){
+    if(i<3){
+        console.log(meses[i]);
+    }
+}
+
